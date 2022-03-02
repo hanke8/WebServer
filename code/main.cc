c3e2b0e6ce208fc73b57e109b7c2e0114dfa6d92
root <root@localhost.localdomain>
2022-03-02 15:58:02 +0800

    finish

--- code/main.cc
+++ code/main.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    WebServer server(1234, 3, 60000, false, 4);
+    WebServer server(1234, 3, 60000, false, 6);
     std::cout << "----------------- start -----------------" << std::endl;
     server.StartService();
     return 0;
